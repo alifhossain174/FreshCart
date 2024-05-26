cf5f9b474df3d0bde7e9bfa5d5de86d9c72665ac
Fahim <alifhossain174@gmail.com>
2024-05-26 23:00:02 +0600

    Updating Code

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index(){
-        return view('index');
+        $sliders = DB::table('banners')->where('type', 1)->where('status', 1)->orderBy('serial', 'asc')->get();
+        return view('index', compact('sliders'));
     }
 }
--- resources/views/homepageSections/sliders.blade.php
+++ resources/views/homepageSections/sliders.blade.php
@@ -1,60 +1,29 @@
-<!-- Slider Section Start -->
 <section class="home-section-2 home-section-bg pt-0 overflow-hidden">
     <div class="container-fluid p-0">
         <div class="row">
             <div class="col-12">
                 <div class="slider-animate">
 
+                    @foreach ($sliders as $slider)
                     <div class="home-contain rounded-0 p-0">
-                        <img src="../assets/images/grocery/banner/1.jpg"
-                            class="img-fluid bg-img blur-up lazyload" alt="" />
+                        <img src="{{env('ADMIN_URL')."/".$slider->image}}" class="img-fluid bg-img blur-up lazyload" alt="" />
                         <div class="home-detail home-big-space p-center-left home-overlay position-relative">
                             <div class="container-fluid-lg">
-                                <div>
-                                    <h6 class="ls-expanded theme-color text-uppercase">
-                                        Weekend Special offer 2
-                                    </h6>
-                                    <h1 class="heding-2">Premium Quality Dry Fruits 2</h1>
-                                    <h2 class="content-2">Dryfruits shopping made Easy</h2>
-                                    <h5 class="text-content">
-                                        Fresh & Top Quality Dry Fruits are available here!
-                                    </h5>
-                                    <button
-                                        class="btn theme-bg-color btn-md text-white fw-bold mt-md-4 mt-2 mend-auto"
-                                        onclick="location.href = 'shop-left-sidebar.html';">
-                                        Shop Now <i class="fa-solid fa-arrow-right icon"></i>
-                                    </button>
-                                </div>
+                                <h6 class="ls-expanded theme-color text-uppercase">{{$slider->sub_title}}</h6>
+                                <h1 class="heding-2">{{$slider->title}}</h1>
+                                {{-- <h2 class="content-2">{{$slider->description}}</h2> --}}
+                                <h5 class="text-content">{{$slider->description}}</h5>
+                                <button class="btn theme-bg-color btn-md text-white fw-bold mt-md-4 mt-2 mend-auto"
+                                    onclick="location.href='{{$slider->btn_link}}'">
+                                    {{$slider->btn_text}} <i class="fa-solid fa-arrow-right icon"></i>
+                                </button>
                             </div>
                         </div>
                     </div>
-                    <div class="home-contain rounded-0 p-0">
-                        <img src="../assets/images/grocery/banner/1.jpg"
-                            class="img-fluid bg-img blur-up lazyload" alt="" />
-                        <div class="home-detail home-big-space p-center-left home-overlay position-relative">
-                            <div class="container-fluid-lg">
-                                <div>
-                                    <h6 class="ls-expanded theme-color text-uppercase">
-                                        Weekend Special offer
-                                    </h6>
-                                    <h1 class="heding-2">Premium Quality Dry Fruits</h1>
-                                    <h2 class="content-2">Dryfruits shopping made Easy</h2>
-                                    <h5 class="text-content">
-                                        Fresh & Top Quality Dry Fruits are available here!
-                                    </h5>
-                                    <button
-                                        class="btn theme-bg-color btn-md text-white fw-bold mt-md-4 mt-2 mend-auto"
-                                        onclick="location.href = 'shop-left-sidebar.html';">
-                                        Shop Now <i class="fa-solid fa-arrow-right icon"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
 
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- Slider Section End -->
